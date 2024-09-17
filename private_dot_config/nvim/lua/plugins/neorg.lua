cffda5c9ba80011985ecaacb43922874f845cd73
Fay Ash <yaoshiu@qq.com>
2024-09-17 08:32:41 +0800

    Update lazy-lock.json Update .config/nvim/lua/plugins/neorg.lua

--- private_dot_config/nvim/lua/plugins/neorg.lua
+++ private_dot_config/nvim/lua/plugins/neorg.lua
@@ -1,5 +1,13 @@
 return {
-  "nvim-neorg/neorg",
-  version = "*",
-  config = true,
+	"nvim-neorg/neorg",
+	version = "*",
+	config = true,
+	opts = {
+		["core.defaults"] = {},
+		["core.concealer"] = {},
+		["core.dirman"] = {
+			default_workspace = "~/neorg",
+		},
+		["core.summary"] = {},
+	},
 }
