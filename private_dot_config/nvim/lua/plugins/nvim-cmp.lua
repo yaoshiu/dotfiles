0001b2a41cd2fc23b744b0282047ad99fa608c40
Fay Ash <yaoshiu@qq.com>
2024-09-17 08:56:55 +0800

    Update .config/nvim/lua/plugins/nvim-cmp.lua

--- private_dot_config/nvim/lua/plugins/nvim-cmp.lua
+++ private_dot_config/nvim/lua/plugins/nvim-cmp.lua
@@ -18,7 +18,7 @@ return {
 			["<Tab>"] = cmp.mapping(function(fallback)
 				if cmp.visible() then
 					-- You could replace select_next_item() with confirm({ select = true }) to get VS Code autocompletion behavior
-					cmp.select_next_item()
+					cmp.confirm({ select = true })
 				elseif vim.snippet.active({ direction = 1 }) then
 					vim.schedule(function()
 						vim.snippet.jump(1)
@@ -42,7 +42,7 @@ return {
 			end, { "i", "s" }),
 			["<CR>"] = cmp.mapping(function(fallback)
 				if cmp.visible() then
-					cmp.confirm({ select = false })
+					cmp.confirm({ select = true })
 				else
 					fallback()
 				end
